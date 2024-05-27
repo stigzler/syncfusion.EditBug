6837eafcd9b55ec9f9b6b1e1da56fd387c412c68
Steve <stigzler@msn.com>
2024-05-27 19:21:02 +0100

    Add project files.

---
+++ syncfusion.EditBug/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Syncfusion.Windows.Edit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syncfusion.EditBug
+{
+    internal class MainWindowViewModel: ObservableObject, INotifyPropertyChanged
+    {
+
+        //  ==============================================================================================
+        //  PROPERTIES
+
+        private string editText;
+        private string editLanguage = "Text";
+        private Languages selectedLanguage = Syncfusion.Windows.Edit.Languages.Text;
+
+        public string EditText { get => editText; set => SetProperty(ref editText, value); }
+        public IEnumerable<Languages> Languages { get => Enum.GetValues(typeof(Languages)).Cast<Languages>(); }
+        public Languages SelectedLanguage { get => selectedLanguage; set => SetProperty(ref selectedLanguage, value); }
+
+        //  ==============================================================================================
+        //  COMMANDS
+
+
+
+
+        public MainWindowViewModel()
+        {
+            EditText = Properties.Resources.SampleXML;
+        }
+
+  
+
+    }
+}
